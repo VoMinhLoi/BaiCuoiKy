c0e5103a9b2e718e3744af7ef780829e10762f3b
HanNhat44 <hannhat2468@gmail.com>
2022-12-01 12:00:53 +0700

    create class product slide and nav

--- app/src/main/java/com/example/quanlyquanao/Activity/MainActivity.java
+++ app/src/main/java/com/example/quanlyquanao/Activity/MainActivity.java
@@ -1,16 +1,83 @@
 package com.example.quanlyquanao.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+import com.example.quanlyquanao.Class.Product;
+import com.example.quanlyquanao.Fragment.CartFragment;
+import com.example.quanlyquanao.Fragment.HistoryFragment;
+import com.example.quanlyquanao.Fragment.ProductFragment;
 import com.example.quanlyquanao.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private AHBottomNavigation ahBotNavHome;
+
+    private List<Product> listCartProduct;
+
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setDataBotNavHome();
     }
-}
+// khởi tạo các item
+    private void initItem() {
+        ahBotNavHome = findViewById(R.id.ahbotnav_home);
+        if(listCartProduct == null){
+            listCartProduct = new ArrayList<>();
+        }
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.contet_frame, new ProductFragment());
+
+        fragmentTransaction.commit();
+    }
+
+    private void setDataBotNavHome() {
+
+        // Create items
+        AHBottomNavigationItem item1 = new AHBottomNavigationItem(R.string.tab_product, R.drawable.ic_baseline_home_24, R.color.teal_200);
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem(R.string.tab_cart, R.drawable.ic_baseline_add_shopping_cart_24, R.color.gray);
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem(R.string.tab_history, R.drawable.ic_baseline_history_24, R.color.yellow);
+
+        // Add items
+        ahBotNavHome.addItem(item1);
+        ahBotNavHome.addItem(item2);
+        ahBotNavHome.addItem(item3);
+
+        ahBotNavHome.setColored(false);
+
+        // Set màu nav
+        ahBotNavHome.setDefaultBackgroundColor(getResources().getColor(R.color.white));
+
+        // Khi click vào các icon trên nav
+        ahBotNavHome.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+            @Override
+            public boolean onTabSelected(int position, boolean wasSelected) {
+
+                switch (position){
+                    case 0:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.contet_frame, new ProductFragment());
+                        fragmentTransaction.commit();
+                        break;
+
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                }
+                return true;
+            }
+        });
+      }
+    }
--- app/src/main/java/com/example/quanlyquanao/Fragment/HistoryFragment.java
+++ app/src/main/java/com/example/quanlyquanao/Fragment/HistoryFragment.java
@@ -12,10 +12,10 @@ import com.example.quanlyquanao.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link history#newInstance} factory method to
+ * Use the {@link HistoryFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class history extends Fragment {
+public class HistoryFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class history extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public history() {
+    public HistoryFragment() {
         // Required empty public constructor
     }
 
@@ -39,8 +39,8 @@ public class history extends Fragment {
      * @return A new instance of fragment history.
      */
     // TODO: Rename and change types and number of parameters
-    public static history newInstance(String param1, String param2) {
-        history fragment = new history();
+    public static HistoryFragment newInstance(String param1, String param2) {
+        HistoryFragment fragment = new HistoryFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
